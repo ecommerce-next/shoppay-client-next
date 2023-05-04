acb842e852124ec3d9c0d5f39c8a22a13c6b3f1a
Irina Gayfulina <63535542+gayfulina@users.noreply.github.com>
2023-05-03 17:17:38 -0700

    swiper

--- components/home/main/index.js
+++ components/home/main/index.js
@@ -1,21 +1,21 @@
 import styles from "./styles.module.scss";
-// import MainSwiper from "./swiper";
-// import Offers from "./offers";
 import { useSession } from "next-auth/react";
+import Header from "./Header";
 import Menu from "./Menu";
+import MainSwiper from "./swiper";
+// import Offers from "./offers";
 // import "swiper/css";
 // import "swiper/css/effect-cards";
 // import User from "./User";
-// import Header from "./Header";
 
 
 export default function Main() {
     const { data: session } = useSession();
     return (
         <div className={styles.main}>
-            {/*<Header />*/}
+            <Header />
             <Menu />
-            {/*<MainSwiper />*/}
+            <MainSwiper />
             {/*<Offers />*/}
             {/*<User />*/}
         </div>
