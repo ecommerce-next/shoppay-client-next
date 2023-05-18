09309b5f80c8049b749a89a64676ef38f84af210
Irina Gayfulina <63535542+gayfulina@users.noreply.github.com>
2023-05-18 14:06:40 -0700

    cart

--- pages/cart.js
+++ pages/cart.js
@@ -1,15 +1,87 @@
-import React from 'react';
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useSession, signIn } from "next-auth/react";
+import {saveCart} from "../requests/user";
+import styles from "../styles/cart.module.scss";
 import Header from "../components/header";
+import CartHeader from "../components/cart/cartHeader";
 
 const country = {
     name: "United State",
         flag: "https://www.seekpng.com/png/full/3-34817_picture-download-american-flag-clipart-no-background-transparent.png",
 };
 const Cart = () => {
+    const Router = useRouter();
+    const { data: session } = useSession();
+    const [selected, setSelected] = useState([]);
+    const { cart } = useSelector((state) => ({ ...state }));
+    const dispatch = useDispatch();
+    //-----------------------
+    const [shippingFee, setShippingFee] = useState(0);
+    const [subtotal, setSubtotal] = useState(0);
+    const [total, setTotal] = useState(0);
+
+    useEffect(() => {
+        setShippingFee(
+            selected.reduce((a, c) => a + Number(c.shipping), 0).toFixed(2)
+        );
+        setSubtotal(selected.reduce((a, c) => a + c.price * c.qty, 0).toFixed(2));
+        setTotal(
+            (
+                selected.reduce((a, c) => a + c.price * c.qty, 0) + Number(shippingFee)
+            ).toFixed(2)
+        );
+    }, [selected]);
+
+    //-----------------------
+    const saveCartToDbHandler = async () => {
+        if (session) {
+            const res = saveCart(selected);
+            Router.push("/checkout");
+        } else {
+            signIn();
+        }
+    };
+
     return (
         <>
-            <Header country={country}/>
-            CART
+            <Header country={country}/><div className={styles.cart}>
+            LALALAL
+
+            {cart.cartItems?.length > 0 ? (
+                <div className={styles.cart__container}>
+                    <CartHeader
+                        cartItems={cart.cartItems}
+                        selected={selected}
+                        setSelected={setSelected}
+                    />
+
+                    {/*<div className={styles.cart__products}>*/}
+                    {/*    {cart.cartItems.map((product) => (*/}
+                    {/*        <Product*/}
+                    {/*            product={product}*/}
+                    {/*            key={product._uid}*/}
+                    {/*            selected={selected}*/}
+                    {/*            setSelected={setSelected}*/}
+                    {/*        />*/}
+                    {/*    ))}*/}
+                    {/*</div>*/}
+                    {/*<Checkout*/}
+                    {/*    subtotal={subtotal}*/}
+                    {/*    shippingFee={shippingFee}*/}
+                    {/*    total={total}*/}
+                    {/*    selected={selected}*/}
+                    {/*    saveCartToDbHandler={saveCartToDbHandler}*/}
+                    {/*/>*/}
+                    {/*<PaymentMethods />*/}
+                </div>
+            ) : (
+                ""
+                // <Empty />
+            )}
+            {/*<ProductsSwiper products={women_swiper} />*/}
+        </div>
         </>
     );
 };
---
+++ utils/arrays_utils.js
@@ -0,0 +1,30 @@
+export const compareArrays = (array1, array2) => {
+    if (array1.length !== array2.length) return false;
+
+    const newArr = (object) =>
+        JSON.stringify(
+            Object.keys(object)
+                .sort()
+                .map((key) => [key, object[key]])
+        );
+
+    array1 = new Set(array1.map(newArr));
+
+    return array2.every((object) => array1.has(newArr(object)));
+};
+
+export const filterArray = (array, property) => {
+    return array
+        .filter((item) => item.name == property)
+        .map((s) => {
+            return s.value;
+        });
+};
+
+export const removeDuplicates = (array) => {
+    return [...new Set(array)];
+};
+
+export const randomize = (array) => {
+    return [...array].sort(() => 0.5 - Math.random());
+};
