c6fd5edaa6cc8e2ec49048bf3cf083b3e4253755
Irina Gayfulina <63535542+gayfulina@users.noreply.github.com>
2023-05-18 12:52:09 -0700

    wip

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import axios from "axios";
 import db from "../utils/db";
-import { useSession } from "next-auth/react";
+import {useSession} from "next-auth/react";
 import styles from "../styles/home.module.scss";
 import Main from "../components/home/main";
 import FlashDeals from "../components/home/flashDeals";
-import { useMediaQuery } from "react-responsive";
+import {useMediaQuery} from "react-responsive";
 import Category from "../components/home/category";
 import {
     women_accessories,
@@ -21,17 +21,17 @@ import Product from "../models/Product";
 import ProductCard from "../components/productCard";
 
 export default function Home({country, products}) {
-    const { data: session } = useSession()
-    const isMedium = useMediaQuery({ query: "(max-width:850px)" });
-    const isMobile = useMediaQuery({ query: "(max-width:550px)" });
+    const {data: session} = useSession()
+    const isMedium = useMediaQuery({query: "(max-width:850px)"});
+    const isMobile = useMediaQuery({query: "(max-width:550px)"});
 
     return (
         <>
-            <Header country={country} />
+            <Header country={country}/>
             <div className={styles.home}>
                 <div className={styles.container}>
-                    <Main />
-                    <FlashDeals />
+                    <Main/>
+                    <FlashDeals/>
 
                     <div className={styles.home__category}>
                         <Category
@@ -78,19 +78,19 @@ export default function Home({country, products}) {
 
                     <div className={styles.products}>
                         {products.map((product) => (
-                            <ProductCard product={product} key={product._id} />
+                            <ProductCard product={product} key={product._id}/>
                         ))}
                     </div>
                 </div>
             </div>
-            <Footer country={country} />
+            <Footer country={country}/>
         </>
     )
 }
 
 export async function getServerSideProps() {
     await db.connectDb();
-    let products = await Product.find().sort({ createdAt: -1 }).lean();
+    let products = await Product.find().sort({createdAt: -1}).lean();
 
     let data = await axios
         .get(`https://api.ipregistry.co/?key=${process.env.GEOLOCATION_KEY}`)
